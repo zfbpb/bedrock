89eba849b63c9e2d6ab1bd503bdcd0d863adcb2e
Zlatko Furčić <zlatko.furcic@gmail.com>
2023-10-04 12:06:35 +0200

    .env.example added in build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,17 @@ echo "Installing Bedrock..."
 composer create-project roots/bedrock .
 echo "DONE."
 
-echo "Copying .env from /build to /html..."
-cp -v ../build/.env .env
-echo "DONE."
+# echo "Copying .env from /build to /html..."
+# cp -v ../build/.env .env
+# echo "DONE."
+
+set -a
+. /usr/local/bin/.env
+set +a
+
+echo "Copying .env from /usr/local/bin to /var/www/html..."
+yes | cp -rf /usr/local/bin/.env /var/www/html
+ln ./build/.env ./html/.env
 
 cd /var/www/html/web/app/themes
 echo "Installing Sage theme..."
@@ -16,7 +24,5 @@ composer create-project roots/sage sage-demo
 echo "DONE."
 
 # echo "Theme path: /html/web/app/themes/${THEME_DIR_NAME}"
-
-
 
 exec "$@"
